eb11c034fe805975af5beb83f46181f4cbb64ce7
Chris Chen <ok3201123@gmail.com>
2023-04-02 18:55:07 +0800

    feat: add colly example

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gocolly/colly/v2"
 	"github.com/line/line-bot-sdk-go/v7/linebot"
 	"github.com/robfig/cron/v3"
 
@@ -87,6 +88,21 @@ func handler(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, ApiRes{})
+}
+
+func crawlPTTPost() {
+	c := colly.NewCollector()
+
+	// Find and visit all links
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit("http://go-colly.org/")
 }
 
 func startCron() {
